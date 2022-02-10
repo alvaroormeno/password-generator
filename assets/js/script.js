93be0d88dda4f75cef48103894e291bc59d73794
alvaroormeno <alvaro@ormeno.org>
2022-02-10 16:38:48 -0500

    basic user length function with prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,39 @@ const numberCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 const specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
+
+// PASSWORD LENGTH FUNCTION
+
+function userLength() {
+
+  var passwordLength = window.prompt("Please input the number of characters you want for your password. MIn 8 - MAX 128 Characters");
+
+  if (passwordLength === null || passwordLength === "") {
+  window.alert("Please provide a valid answer!")
+  return userLength();
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("The password can only have a MINIMUM of 8 characterd and a MAXIMUM of 128 characters! Please provide a valid answer!");
+    return userLength()
+  }
+  console.log(passwordLength);
+  //return parseInt(passwordLength);
+
+  
+  
+
+};
+
+
+
+
+
+userLength();
+
+
+
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
